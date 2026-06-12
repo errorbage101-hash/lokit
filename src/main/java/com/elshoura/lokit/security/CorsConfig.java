f41a139c7994559d5fd68c139a4db44b9f708769
mohamed <mohamedelshoura337@gmail.com>
2026-06-12 04:07:18 +0300

    add localhot for cors config

--- src/main/java/com/elshoura/lokit/security/CorsConfig.java
+++ src/main/java/com/elshoura/lokit/security/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(List.of(
                 "https://new-lokit.vercel.app",
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "http://localhost:8080/"
         ));
 
         configuration.setAllowedMethods(List.of("*"));
